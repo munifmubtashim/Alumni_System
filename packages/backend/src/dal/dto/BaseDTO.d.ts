49e715ac606aadcbee85f9aa28b50df2e08c18ca
munifmubtashim <munifmubtasim@gmail.com>
2026-06-13 10:01:52 +0600

    Remove obsolete BaseDTO and PostDTO files; update PostManager to utilize PostQuery for post creation

--- packages/backend/src/dal/dto/BaseDTO.d.ts
+++
@@ -1,4 +0,0 @@
-export interface BaseDTO {
-    id: Number;
-}
-//# sourceMappingURL=BaseDTO.d.ts.map
--- packages/businessLogic/src/PostManager.ts
+++ packages/businessLogic/src/PostManager.ts
@@ -1,13 +1,30 @@
-import { PostDTO } from "../../backend/src/dal/dto/PostDTO.js";
+import { PostDTO } from "../../backend/src/dal/dto/PostDTO";
+import {PostQuery} from "../../backend/src/dal/query/PostQuery"
 
 
 
-export class PostManager{
+export class PostManager {
+  public async createNewPost(
+    user_id: number,
+    caption: string,
+    media_url: string,
+    comments_count: number,
+   
+  ) {
+    const postQuery = new PostQuery();
+    const post = new PostDTO(
+      user_id,
+      comments_count,
+      caption,
+      media_url,
+    );
+    const newPost = postQuery.createPost(post);
+    console.log(newPost);
+  }
 
-    public async createPost(post:PostDTO){
 
 
-    }
+    
  public async updatePost(post:PostDTO){
 
  }
