9cbc482c9aa5dfe76c4328afbfaf979327535565
munifmubtashim <munifmubtasim@gmail.com>
2026-06-20 10:29:21 +0600

    feat: add undici-types package with comprehensive type definitions
    - Introduced MockPool class for mocking requests with intercept capabilities.
    - Added Pool and RoundRobinPool classes for managing HTTP connections.
    - Implemented PoolStats class to provide statistics on pool connections.
    - Created SnapshotAgent and SnapshotRecorder for recording and managing request/response snapshots.
    - Added Socks5ProxyAgent for handling SOCKS5 proxy connections.
    - Developed utility types and functions for better type handling in undici.
    - Included WebSocket and related event classes for WebSocket handling.
    - Enhanced retry mechanism with RetryAgent and RetryHandler classes.
    - Added various utility types and interfaces for better type safety and usability.

--- packages/backend/src/businessLogic/src/TestManager.ts
+++ packages/backend/src/businessLogic/src/TestManager.ts
@@ -5,73 +5,73 @@ import { AlumniManager } from "./AlumniManager";
 import { UserManager } from "./UserManager";
 
 
-const postManager = new PostManager();
-const post = new PostDTO(1, 0, "Hello World", "https://image.com/photo.jpg");
-postManager.createNewPost(post);
-postManager.getAllPosts();
-const postById = new PostDTO(1, 0);
-postManager.getPostsByUserId(postById);
-const updatePost = new PostDTO(
-  1,
-  0,
-  "Updated Caption",
-  "https://image.com/new.jpg",
-);
-updatePost.id = 1;
-postManager.updatePost(updatePost);
-const commentPost = new PostDTO(1, 5);
-commentPost.id = 1;
-postManager.updateCommentCount(commentPost);
-const deletePost = new PostDTO(1, 0);
-deletePost.id = 1;
-postManager.deletePost(deletePost);
+// const postManager = new PostManager();
+// const post = new PostDTO(1, 0, "Hello World", "https://image.com/photo.jpg");
+// postManager.createNewPost(post);
+// postManager.getAllPosts();
+// const postById = new PostDTO(1, 0);
+// postManager.getPostsByUserId(postById);
+// const updatePost = new PostDTO(
+//   1,
+//   0,
+//   "Updated Caption",
+//   "https://image.com/new.jpg",
+// );
+// updatePost.id = 1;
+// postManager.updatePost(updatePost);
+// const commentPost = new PostDTO(1, 5);
+// commentPost.id = 1;
+// postManager.updateCommentCount(commentPost);
+// const deletePost = new PostDTO(1, 0);
+// deletePost.id = 1;
+// postManager.deletePost(deletePost);
 
 
-const commentManager = new CommentManager();
-const comment = new CommentDTO(1, 1, 1, "Nice post!");
-commentManager.createComment(comment);
-commentManager.getAllComments();
-const updateComment = new CommentDTO(1, 1, 1, "Updated comment!");
-updateComment.id = 1;
-commentManager.updateComment(updateComment);
-const deleteComment = new CommentDTO(1, 1, 1, "");
-deleteComment.id = 1;
-commentManager.deleteComment(deleteComment);
+// const commentManager = new CommentManager();
+// const comment = new CommentDTO(1, 1, 1, "Nice post!");
+// commentManager.createComment(comment);
+// commentManager.getAllComments();
+// const updateComment = new CommentDTO(1, 1, 1, "Updated comment!");
+// updateComment.id = 1;
+// commentManager.updateComment(updateComment);
+// const deleteComment = new CommentDTO(1, 1, 1, "");
+// deleteComment.id = 1;
+// commentManager.deleteComment(deleteComment);
 
 
-const alumniManager = new AlumniManager();
-const alumni = new AlumniDTO(
-  1,
-  1,
-  "Computer Science",
-  2023,
-  "Google",
-  "Engineer",
-  "5 years",
-  "I am an alumni",
-  "https://linkedin.com/in/alumni",
-);
-alumniManager.createAlumni(alumni);
-alumniManager.getAllAlumni();
-alumniManager.findAlumniById(1);
-alumniManager.findAlumniByEmail("alumni@email.com");
-alumniManager.updateAlumni(1, { department: "Software Engineering" });
+// const alumniManager = new AlumniManager();
+// const alumni = new AlumniDTO(
+//   1,
+//   1,
+//   "CSE",
+//   2023,
+//   "Google",
+//   "Engineer",
+//   "5 years",
+//   "I am an alumni",
+//   "https://linkedin.com/in/alumni",
+// );
+// alumniManager.createAlumni(alumni);
+// alumniManager.getAllAlumni();
+// alumniManager.findAlumniById(1);
+// alumniManager.findAlumniByEmail("alumni@email.com");
+// alumniManager.updateAlumni(1, { department: "Software Engineering" });
 
 
 
-const userManager = new UserManager();
-const user = new UserDTO(
-  "Munif Mubtashim",
-  "munifmubtashim@gmail.com",
-  "password123",
-  "alumni",
-  "https://image.com/photo.jpg",
-);
-userManager.createUser(user);
-userManager.getAllUsers();
-userManager.findUserById(1);
-userManager.findUserByEmail("munifmubtashim@gmail.com");
-userManager.updateUser(1, { name: "Updated" });
-userManager.updateLoginTime(1);
-userManager.updateLogoutTime(1);
-userManager.deleteUser(1);
+// const userManager = new UserManager();
+// const user = new UserDTO(
+//   "Munif Mubtashim",
+//   "munifmubtashim@gmail.com",
+//   "password123",
+//   "alumni",
+//   "https://image.com/photo.jpg",
+// );
+// userManager.createUser(user);
+// userManager.getAllUsers();
+// userManager.findUserById(1);
+// userManager.findUserByEmail("munifmubtashim@gmail.com");
+// userManager.updateUser(1, { name: "Updated" });
+// userManager.updateLoginTime(1);
+// userManager.updateLogoutTime(1);
+// userManager.deleteUser(1);
